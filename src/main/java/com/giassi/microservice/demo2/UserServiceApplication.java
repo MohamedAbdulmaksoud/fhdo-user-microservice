e2edd66ea3c106ba4a2d1b4f1505331c5921436f
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2023-09-24 13:02:09 +0200

    rename service

--- src/main/java/com/giassi/microservice/demo2/UserServiceApplication.java
+++ src/main/java/com/giassi/microservice/demo2/UserServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Microservice2Application {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Microservice2Application.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
